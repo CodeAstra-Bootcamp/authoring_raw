5219304a4c486069e517e6ff42e575b5d028bee6
B V Satyaram <bvsatyaram@gmail.com>
2016-04-26 17:47:59 +0530

    CRUD of Chapters

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :books
+  resources :books do
+    resources :chapters
+  end
 
   # Example resource route with options:
   #   resources :products do
