5de92145f26329b0165e9366d8e06559c337b7ff
B V Satyaram <bvsatyaram@gmail.com>
2016-04-26 16:47:18 +0530

    Add and delete books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,36 @@
 class BooksController < ApplicationController
+  before_action :fetch_book, only: [:show, :destroy]
+
   def index
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to root_path, notice: "Book deleted successfully"
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :description)
+  end
+
+  def fetch_book
+    @book = Book.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if Rails.env.development?
+  Book.find_or_create_by(title: "How to play guitar") do |book|
+    book.description = "Learn to play guitar in 24 hours"
+  end
+  Book.find_or_create_by(title: "How to be a good developer") do |book|
+    book.description = "What you need to know before your friends start envying you"
+  end
+  Book.find_or_create_by(title: "How to live in India") do |book|
+    book.description = "Things for foreighers to know before entering India"
+  end
+  Book.find_or_create_by(title: "Photography") do |book|
+    book.description = "The ins and outs of photography, to the point"
+  end
+end
